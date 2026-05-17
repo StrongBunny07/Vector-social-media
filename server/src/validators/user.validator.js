02998697b0f1fc9d0991afe43dd3c7f6a9cd8b59
StrongBunny <17nandha2004@gmail.com>
2026-05-17 19:01:24 +0530

    fix : updated bio validation . bio is optional now

--- server/src/validators/user.validator.js
+++ server/src/validators/user.validator.js
@@ -18,7 +18,7 @@ export const registerSchema = z.object({
     }),
   password: z.string({ required_error: "Password must be at least 6 characters!" }).min(6, { message: "Password must be at least 6 characters!" }),
   username: z.string({ required_error: "Please enter a username!" }).trim().min(1, { message: "Please enter a username!" }),
-  bio: z.string({ required_error: "Please enter a bio!" }).trim().min(1, { message: "Please enter a bio!" }),
+  bio: z.string({ required_error: "Please enter a bio!" }).trim().optional(),
   description: z.string({ required_error: "Please enter a description!" }).trim().min(1, { message: "Please enter a description!" }),
   isPrivate: z.boolean().optional().default(false),
 });
